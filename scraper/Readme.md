54411b5c8361a63eef39326d499083271b8a23ec
Jan Hein de Jong <janhein.dejong@gmail.com>
2022-05-27 16:32:23 +0200

    Added scraper to compose

--- scraper/Readme.md
+++ scraper/Readme.md
@@ -2,9 +2,8 @@
 
 Application that does the actually scraping or snapshotting of URLs 
 
-## Deployment 
+Environment variables:  
 
-I'm not really sure what is the best way to actually run this, but here's a few options: 
-
-- Cronjob 
-- Watchcommand
+- `APP_CONFIG_RESOURCE_API`
+- `APP_CONFIG_SNAPSHOT_API`
+- `APP_CONFIG_WAIT_DURATION`
--- scraper/main.go
+++ scraper/main.go
@@ -6,28 +6,40 @@ package main
 
 import (
 	"log"
+	"os"
 	"sync"
+	"time"
 )
 
 func main() {
+
+	config := LoadConfigFromEnv()
+
 	// Create services
-	ResourceApiService := ResourceApiService{host: "http://resources"}
-	SnapShotApiService := SnapShotApiService{host: "http://snapshots"}
+	ResourceApiService := ResourceApiService{host: config.ResourceApi}
+	SnapShotApiService := SnapShotApiService{host: config.SnapShotApi}
 
-	// Get all Resources from resource api service
-	resources, err := ResourceApiService.GetResources()
+	waitTime, err := time.ParseDuration(config.WaitDuration)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatalf(`Couldn't parse duration string "%s"`, getEnvWithDefault("APP_CONFIG_WAIT_TIME", "30s"))
 	}
+	for {
+		// Get all Resources from resource api service
+		resources, err := ResourceApiService.GetResources()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
 
-	// For every resource, launch goroutine that scrapes and posts to snapshot service
-	var wg sync.WaitGroup
-	for _, resource := range *resources {
-		wg.Add(1)
-		resource := resource
-		go SnapAndSave(&wg, &resource, &SnapShotApiService)
+		// For every resource, launch goroutine that scrapes and posts to snapshot service
+		var wg sync.WaitGroup
+		for _, resource := range *resources {
+			wg.Add(1)
+			resource := resource
+			go SnapAndSave(&wg, &resource, &SnapShotApiService)
+		}
+		wg.Wait()
+		time.Sleep(waitTime)
 	}
-	wg.Wait()
 }
 
 // Takes snapshot of resource, and stores to snapshot database
@@ -45,4 +57,28 @@ func SnapAndSave(wg *sync.WaitGroup, resource *Resource, snapShotApiService *Sna
 		return
 	}
 	log.Printf(`Successfully stored snapshot of resource with URL "%s"`, resource.Url)
+}
+
+func getEnvWithDefault(key string, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		value = defaultValue
+	}
+	return value
+}
+
+type Config struct {
+	ResourceApi  string
+	SnapShotApi  string
+	WaitDuration string
+}
+
+func LoadConfigFromEnv() *Config {
+	config := Config{
+		ResourceApi:  getEnvWithDefault("APP_CONFIG_RESOURCE_API", "http://resources"),
+		SnapShotApi:  getEnvWithDefault("APP_CONFIG_SNAPSHOT_API", "http://snapshots"),
+		WaitDuration: getEnvWithDefault("APP_CONFIG_WAIT_DURATION", "30s"),
+	}
+	log.Printf(`Loaded configuration: %+v`, config)
+	return &config
 }
