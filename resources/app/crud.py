1b940d30b6952a5e741c7990008558452457e91d
Jan Hein de Jong <janhein.dejong@gmail.com>
2022-06-04 11:13:34 +0000

    Add delete functionality to resource app

--- resources/app/crud.py
+++ resources/app/crud.py
@@ -17,3 +17,12 @@ def create_new_resource(db: Session, obj_in: schemas.ResourceBase) -> models.Res
     db.commit()
     db.refresh(resource)
     return resource
+
+
+def delete_resource(db: Session, resource_id: int) -> None:
+    n_deleted_rows = (
+        db.query(models.Resource).filter(models.Resource.id == resource_id).delete()
+    )
+    if n_deleted_rows == 0:
+        raise ResourceNotFound()
+    db.commit()
--- resources/app/main.py
+++ resources/app/main.py
@@ -1,6 +1,6 @@
 """Hello, world"""
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Response, status
 from sqlalchemy.orm import Session
 
 from . import crud, database, models, schemas
@@ -33,3 +33,12 @@ def post_resource(
     resource: schemas.ResourceBase, db: Session = Depends(get_db)
 ) -> models.Resource:
     return crud.create_new_resource(db, resource)
+
+
+@app.delete("/resource")
+def delete_resource(resource_id: int, db: Session = Depends(get_db)) -> models.Resource:
+    try:
+        crud.delete_resource(db, resource_id)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    except crud.ResourceNotFound:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
